1c6c2e4d6b03d87db48f4cf4eb9bc7cf49e7bce2
zux <zux.grr@gmail.com>
2024-02-12 08:38:08 +0500

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,29 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+
+    private List<T> data;
+    private int index;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        index = (index == data.size() ? 0 : index);
+        return !data.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data.get(index++);
+    }
+}
