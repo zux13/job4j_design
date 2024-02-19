3ecdf5b2f6682bd3008fb22147e22ae3fb47f315
zux <zux.grr@gmail.com>
2024-02-19 10:29:52 +0500

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,82 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size() == container.length) {
+            container = grow();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size());
+        T oldValue = container[index];
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size());
+        T oldValue = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return oldValue;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size());
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            final int expectedModCount = modCount;
+            int pointer;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return pointer < size();
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[pointer++];
+            }
+        };
+    }
+
+    private T[] grow() {
+        int newLength = (container.length == 0 ? 1 : container.length) * 2;
+        return Arrays.copyOf(container, newLength);
+    }
+}
