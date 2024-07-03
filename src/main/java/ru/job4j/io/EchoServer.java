1d70667afb113c293004d07c827a2bcfada05113
zux <zux.grr@gmail.com>
2024-07-03 08:04:14 +0500

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,8 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -10,24 +12,40 @@ public class EchoServer {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 boolean exitMsg;
-                try (OutputStream output = socket.getOutputStream();
+                try (BufferedWriter output = new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream()));
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
                     String startingLine = input.readLine();
-                    exitMsg = startingLine.contains("?msg=Bye");
-                    System.out.println(startingLine);
+                    String message = extractMessage(startingLine);
+                    exitMsg = "Exit".equals(message);
 
+                    output.write("HTTP/1.1 200 OK\r\n\r\n");
+                    output.write(exitMsg ? "Shutting down... Bye bye" : message);
+                    output.flush();
+
+                    System.out.println(startingLine);
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
                     }
-                    output.flush();
                 }
                 if (exitMsg) {
                     server.close();
                 }
             }
         }
+    }
+
+    private static String extractMessage(String startingLine) {
+
+        Pattern pattern = Pattern.compile("GET /\\?msg=([^ ]+) HTTP/1\\.1");
+        Matcher matcher = pattern.matcher(startingLine);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return "";
     }
 }
