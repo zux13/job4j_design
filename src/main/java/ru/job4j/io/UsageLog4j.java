60317c43d1d447d4b3987b798000db8174fa0d75
zux <zux.grr@gmail.com>
2024-07-03 09:27:11 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte userId = 10;
+        short numOfFriends = 200;
+        int numOfMessages = 1500;
+        long accountBalance = 500000L;
+        float userRating = 4.75F;
+        double totalSpent = 12345.67;
+        char initial = 'P';
+        boolean isActive = true;
+
+        LOG.debug("User ID: {}, Number of Friends: {}, Number of Messages: {}, Account Balance: {}, User Rating: {},"
+                        + "Total Spent: {}, Initial: {}, Active: {}",
+                userId, numOfFriends, numOfMessages, accountBalance, userRating, totalSpent, initial, isActive);
     }
 }
