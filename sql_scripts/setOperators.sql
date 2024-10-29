71621517444c76e28929918d49fbc581225a6ad9
zux <zux.grr@gmail.com>
2024-10-29 10:29:42 +0500

    10. Операторы множества в SQL [#504896]

---
+++ sql_scripts/setOperators.sql
@@ -0,0 +1,52 @@
+CREATE TABLE movie
+(
+    id       SERIAL PRIMARY KEY,
+    name     text,
+    director text
+);
+
+CREATE TABLE book
+(
+    id     SERIAL PRIMARY KEY,
+    title  text,
+    author text
+);
+
+INSERT INTO movie (name, director)
+VALUES ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+INSERT INTO book (title, author)
+VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+	   
+SELECT name 
+FROM movie
+INTERSECT
+SELECT title
+FROM book;
+
+SELECT title 
+FROM book
+EXCEPT
+SELECT name
+FROM movie;
+
+(SELECT name 
+FROM movie
+EXCEPT
+SELECT title
+FROM book)
+UNION ALL
+(SELECT title 
+FROM book
+EXCEPT
+SELECT name
+FROM movie);
+
